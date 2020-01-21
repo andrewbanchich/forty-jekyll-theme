bc52c9af2fc704561bda3237f1d6f9ba0ddf88d6
LadderOperator <tz_ji@qq.com>
2020-01-21 14:36:54 +0800

    进一步优化了排版

--- tools/mdcvt/js/index.js
+++ tools/mdcvt/js/index.js
@@ -1991,15 +1991,15 @@ showdown.subParser('italicsAndBold', function (text, options, globals) {
     //underscores
     // Since we are consuming a \s character, we need to add it
     text = text.replace(/(^|\s|>|\b)__(?=\S)([\s\S]+?)__(?=\b|<|\s|$)/gm, '$1<strong>$2</strong>');
-    text = text.replace(/(^|\s|>|\b)_(?=\S)([\s\S]+?)_(?=\b|<|\s|$)/gm, '$1<em>$2</em>');
+    text = text.replace(/(^|\s|>|\b)_(?=\S)([\s\S]+?)_(?=\b|<|\s|$)/gm, '$1<em class="comment">$2</em>');
     //asterisks
     text = text.replace(/(\*\*)(?=\S)([^\r]*?\S[*]*)\1/g, '<strong>$2</strong>');
-    text = text.replace(/(\*)(?=\S)([^\r]*?\S)\1/g, '<em>$2</em>');
+    text = text.replace(/(\*)(?=\S)([^\r]*?\S)\1/g, '<em class="comment">$2</em>');
 
   } else {
     // <strong> must go first:
     text = text.replace(/(\*\*|__)(?=\S)([^\r]*?\S[*_]*)\1/g, '<strong>$2</strong>');
-    text = text.replace(/(\*|_)(?=\S)([^\r]*?\S)\1/g, '<em>$2</em>');
+    text = text.replace(/(\*|_)(?=\S)([^\r]*?\S)\1/g, '<em class="comment">$2</em>');
   }
 
   text = globals.converter._dispatch('italicsAndBold.after', text, options, globals);
