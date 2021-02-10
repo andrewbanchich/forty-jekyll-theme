b95be9998664c42ca4d502bfcfaaacf2bbc7941a
Yoonsang Lee <yslee111@gmail.com>
2021-02-10 09:12:20 +0900

    Update SIGGRAPH 2020 paper info

--- publications-eng.js
+++ publications-eng.js
@@ -5,7 +5,7 @@ var publications_eng = [
 		'conference_journal': 'SIGGRAPH',
 		'year': 2020,
 		'authors': 'Taesoo Kwon, Yoonsang Lee, Michiel van de Panne',
-		'conference_journal_full': 'ACM Transactions on Graphics (SIGGRAPH 2020)',
+		'conference_journal_full': 'ACM Transactions on Graphics (SIGGRAPH 2020), Volume 39 Issue 4, Article No. 46, July 2020',
 		'video_iframe': '<iframe width="1280" height="749" src="https://www.youtube.com/embed/fGwMrRoC5bw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
 		'project_page': 'http://calab.hanyang.ac.kr/papers/flexLoco.html',
 		'type': 'paper',
