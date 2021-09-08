e740b5b10d93e800de83dc9819bba23ff7861511
Tom Garbutt <tom@tcsg.uk>
2021-09-08 14:47:04 +0100

    Fix menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -321,7 +321,7 @@
 					$body.removeClass('is-menu-visible');
 
 				})
-				.append('<a class="close" href="#menu">Close</a>');
+				.append('<a class="close" title="close menu" href="#menu">Close</a>');
 
 			$body
 				.on('click', 'a[href="#menu"]', function(event) {
